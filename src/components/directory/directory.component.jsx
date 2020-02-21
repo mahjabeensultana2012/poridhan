af7e22942afa2958c826bfbfff35bd0dcd341f00
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-21 13:53:21 -0600

    add image in background

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -40,8 +40,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title }) => (
-          <MenuItem key={id} title={title} />
+        {this.state.sections.map(({ id, title, imageUrl }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} />
         ))}
       </div>
     );
