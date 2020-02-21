c1bcd7e39675289210ccee6a4bcb6fd9973a9d80
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-21 12:57:41 -0600

    implement homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './homepage.component';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      Welcome to shop
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
