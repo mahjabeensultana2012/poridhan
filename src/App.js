d6e3005c5e8723d214df0815db0b1c9dcbb75b17
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-27 10:50:24 -0600

    signin with google auth and unsubscribeFromAuth

--- src/App.js
+++ src/App.js
@@ -14,12 +14,17 @@ class App extends React.Component {
       currentUser: null,
     };
   }
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
       console.log('uuu', user);
     });
+  }
+
+  componentWillUpdate() {
+    this.unsubscribeFromAuth();
   }
   render() {
     return (
