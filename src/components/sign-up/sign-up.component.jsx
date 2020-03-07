5682ca8a8778bd9d82ee5e74d0f67651df64dfce
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-03-06 19:19:33 -0600

    implement sign-up component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+
+  handleSubmit = () => {};
+
+  renser() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I don't have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            label="Display Name"
+            onChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            label="Email"
+            onChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            label="Password"
+            onChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            label="Confirm Password"
+            onChange={this.handleChange}
+            required
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
