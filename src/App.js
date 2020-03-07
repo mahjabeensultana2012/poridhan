6d199b46bb49c0b14e2eb2300ada80813ab3bb57
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-03-07 11:00:58 -0600

    sign-in data is stored in firebase

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log('state', this.state);
+          //console.log('state', this.state);
         });
       }
       this.setState({
