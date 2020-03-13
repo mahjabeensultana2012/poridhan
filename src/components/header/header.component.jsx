d6a975c620218e5a43eca4a3870a0f8c330a3f38
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-03-13 10:18:07 -0500

    implement CartDropDown into header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropDown />
   </div>
 );
 
