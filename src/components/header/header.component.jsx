dad3db91fa03a51d142c1b85e670fb80e6f5ba0b
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-03-17 12:16:35 -0500

    toggle cartIcon is not working in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -30,7 +30,7 @@ const Header = ({ currentUser, hidden }) => (
       )}
       <CartIcon />
     </div>
-    {hidden ? null : <CartDropDown />}
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
