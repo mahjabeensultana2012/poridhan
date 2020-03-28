1e77cd85f0a80413e86ea3e1a4868ab2ef58a787
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-03-27 19:08:37 -0500

    modify styles for collection-item

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './custom-button.styles.scss';
 
 const CustomButton = ({
@@ -9,9 +8,9 @@ const CustomButton = ({
   ...otherProps
 }) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} ${
-      inverted ? 'inverted' : ''
-    }custom-button`}
+    className={` ${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
